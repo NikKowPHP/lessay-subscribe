84ceca79ee59d258dd4059d995acd7c502c287f3
NikKowPHP <nik.kow@outlook.com>
2025-02-21 11:07:40 +0100

    fix the layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,7 +73,7 @@ export default function Home() {
   const jsonLd = generateJsonLd();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-2 sm:p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-2 sm:p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)] max-w-4xl mx-auto">
       <main className="flex flex-col gap-8 row-start-2 items-center max-w-full sm:max-w-[1000px]">
         <div className="text-center">
           <h1 className="text-4xl sm:text-5xl font-bold mb-4">
