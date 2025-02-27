a8a950213168867b6c02ab06f7489e7c1791790d
NikKowPHP <nik.kow@outlook.com>
2025-02-27 09:36:10 +0100

    implmement tts interface

--- src/services/tts.service.ts
+++ src/services/tts.service.ts
@@ -1,25 +1,13 @@
+import { ITTS } from "@/interfaces/tts.interface";
 
 export class TTS {
 
-  constructor() {
-    this.polly = new PollyService();
+  private ttsEngine: ITTS;
+  constructor(ttsEngine: ITTS) {
+    this.ttsEngine = ttsEngine;
   }
 
-  async fetchPollyAudio(ipa: string, language: string, voice: string) {
-    return await fetchPollyAudio(ipa, language);
+  public async generateAudio(text: string, language: string, voice: string) {
+    return await this.ttsEngine.synthesizeSpeech(text, language, voice);
   }
-  private getConfig(language: string, voice: string) {
-
-    AWS.config.update({
-    accessKeyId: process.env.AWS_POLLY_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_POLLY_SECRET_KEY,
-    region: process.env.AWS_POLLY_REGION
-  });
-    return {
-      languageCode: language,
-      voiceId: voice
-    }
-  }
-
-
 }
