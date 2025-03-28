dc148a6c2772239ee2d754122c3a92c8d0126996
NikKowPHP <nik.kow@outlook.com>
2025-03-28 11:28:51 +0100

    add uploading to vercel functionality

--- src/hooks/use-upload.tsx
+++ src/hooks/use-upload.tsx
@@ -2,13 +2,15 @@ import { useCallback } from "react";
 import { upload } from "@vercel/blob/client";
 import logger from "@/utils/logger";
 
+
+
 export const useUpload = () => {
   const uploadFile = useCallback(
     async (file: File, pathPrefix: string): Promise<string> => {
       const filename = `${pathPrefix}/${Date.now()}-${file.name}`;
       const blob = await upload(filename, file, {
         access: "public",
-        handleUploadUrl: "/api/admin/upload-token",
+        handleUploadUrl: "/api/upload-token",
         clientPayload: JSON.stringify({
           __development__: "bypass-auth-for-localhost",
         }),
