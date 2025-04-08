8dce209792d03fe2edc2dc8e7377827def0c433d
NikKowPHP <nik.kow@outlook.com>
2025-04-08 13:24:00 +0200

    loose up the linter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,26 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "no-unused-vars": "off",
+      "@typescript-eslint/no-unused-vars": "off",
+      "no-console": "off",
+      "no-undef": "off",
+      "no-empty": "off",
+      "no-extra-semi": "off",
+      "no-mixed-spaces-and-tabs": "off",
+      "no-redeclare": "off",
+      "no-unreachable": "off",
+      "no-unsafe-finally": "off",
+      "no-unsafe-negation": "off",
+      "no-useless-escape": "off",
+      "no-var": "off",
+      "prefer-const": "off",
+      "quotes": "off",
+      "semi": "off"
+    }
+  }
 ];
 
 export default eslintConfig;
